f95602447f29856d8631f26e90e45c33b7ed05ac
Emil J. Tywoniak <emil@tywoniak.eu>
2023-05-01 22:56:58 +0200

    Basic repulsion forces

---
+++ README.md
@@ -0,0 +1,3 @@
+# PrettyPlace
+
+I'm making a non-linear placer for fun. Here it is. Stare at the code, if you'd like!
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,99 @@
-use nannou::prelude::{App, Closed, Draw, Event, Frame, Update};
+// use itertools::izip;
 use nannou::color::*;
+use nannou::prelude::{App, Closed, Draw, Event, Frame, Update};
+// use ndarray::prelude::*;
+// use ndarray_linalg::Scalar;
 use rand::Rng;
 use std::time::Duration;
-use std::vec::Vec;
-use ndarray::prelude::*;
+// use std::vec::Vec;
 
-struct Charge {
-    r: nannou::prelude::Vec2,
-    q: f32,
+#[derive(Clone, Copy, Default)]
+struct Loc {
+    x: f32,
+    y: f32,
+}
+impl std::ops::Div<Self> for Loc {
+    type Output = Loc;
+    fn div(self, other: Self) -> Loc {
+        return Loc {
+            x: self.x / other.x,
+            y: self.y / other.y,
+        };
+    }
+}
+impl std::ops::Mul<Self> for Loc {
+    type Output = Loc;
+    fn mul(self, other: Self) -> Loc {
+        return Loc {
+            x: self.x * other.x,
+            y: self.y * other.y,
+        };
+    }
+}
+impl std::ops::Mul<f32> for Loc {
+    type Output = Loc;
+    fn mul(self, other: f32) -> Loc {
+        return Loc {
+            x: self.x * other,
+            y: self.y * other,
+        };
+    }
+}
+impl std::ops::Div<f32> for Loc {
+    type Output = Loc;
+    fn div(self, other: f32) -> Loc {
+        return Loc {
+            x: self.x / other,
+            y: self.y / other,
+        };
+    }
+}
+impl std::ops::Add for Loc {
+    type Output = Loc;
+    fn add(self, other: Loc) -> Loc {
+        return Loc {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        };
+    }
+}
+impl std::ops::Sub for Loc {
+    type Output = Loc;
+    fn sub(self, other: Loc) -> Loc {
+        return Loc {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        };
+    }
+}
+impl nannou::prelude::Zero for Loc {
+    fn zero() -> Self {
+        return Loc { x: 0.0, y: 0.0 };
+    }
+    fn is_zero(&self) -> bool {
+        return self.x == 0.0 && self.y == 0.0;
+    }
+}
+
+#[derive(Clone, Default)]
+struct Charges {
+    loc: ndarray::Array1<Loc>,
+    q: ndarray::Array1<f32>,
 }
 
 struct Model {
-    charges: Vec<Charge>,
+    charges: Charges,
     sim_delta: std::time::Duration,
     origin: nannou::prelude::Vec2,
-    matrix: ndarray::Array2<f32>,
+    matrix_x: ndarray::Array2<f32>,
+    matrix_y: ndarray::Array2<f32>,
+    // slow: bool,
 }
 
-const SIM_DELTA: Duration = Duration::new(0, 10_000);
+// TODO make this less global
+const SIM_DELTA: Duration = Duration::new(0, 20_000);
 const JUMP: f32 = 1.0;
-const NUM_CHARGES: usize = 40;
+const NUM_CHARGES: usize = 100;
 const W: f32 = 600.0;
 const H: f32 = 600.0;
 const X_RES: i64 = 16;
@@ -34,26 +108,53 @@ fn main() {
 
 fn model(_app: &App) -> Model {
     let mut rng = rand::thread_rng();
-    let mut charges: Vec<Charge> = Vec::new();
-    for _ in 0..NUM_CHARGES {
-        charges.push(Charge {
-            r: nannou::prelude::Vec2::new(rng.gen_range(0.0..W), rng.gen_range(0.0..H)),
-            q: 3.0,
-        })
+    let mut loc: ndarray::Array1<Loc> = ndarray::Array1::zeros(NUM_CHARGES);
+    for l in &mut loc {
+        l.x = rng.gen_range(0.0, W);
+        l.y = rng.gen_range(0.0, H);
     }
+    let q = ndarray::Array::from_elem(NUM_CHARGES, 3.0);
     Model {
-        charges,
+        charges: Charges {
+            loc: loc,
+            q: q,
+        },
         sim_delta: std::time::Duration::ZERO,
-        origin: nannou::prelude::Vec2::new(- W / 2.0, - H / 2.0),
-        matrix: ndarray::Array2::zeros((NUM_CHARGES, NUM_CHARGES)),
+        origin: nannou::prelude::Vec2::new(-W / 2.0, -H / 2.0),
+        matrix_x: ndarray::Array2::zeros((NUM_CHARGES, NUM_CHARGES)),
+        matrix_y: ndarray::Array2::zeros((NUM_CHARGES, NUM_CHARGES)),
+        // slow: false,
     }
 }
 
-fn move_charges(charges: &mut Vec<Charge>) {
-    for charge in charges {
-        charge.r.x += 1.0;
-        charge.r.x = charge.r.x.max(0.0).min(W); //TODO
-        charge.r.y = charge.r.y.max(0.0).min(H);
+fn dir_dist(a: &Loc, b: &Loc) -> Loc {
+    let ddd = *a - *b;
+    let sq: f32 = ddd.x.powf(2.0) + ddd.y.powf(2.0);
+    assert!(sq != 0.0);
+    return ddd/sq;
+}
+fn update_matrix(m: &mut Model) {
+    let it = m.charges.loc.iter().zip(m.charges.q.iter()).enumerate();
+    for (i, (c_i_loc, c_i_q)) in it.clone() {
+        for (j, (c_j_loc, _)) in it.clone() {
+            // println!("i {i} j {j}");
+            if i == j {
+                continue;
+            }
+            let q = Loc {x: *c_i_q, y: *c_i_q};
+            let d = dir_dist(c_i_loc, c_j_loc);
+            m.matrix_x[[i, j]] = (q*d).x;
+            m.matrix_y[[i, j]] = (q*d).y;
+        }
+    }
+}
+
+fn move_charges(m: &mut Model) {
+    let add_x = m.matrix_x.dot(&m.charges.q);
+    let add_y = m.matrix_y.dot(&m.charges.q);
+    for (i, loc) in &mut m.charges.loc.iter_mut().enumerate() {
+        loc.x = (loc.x + add_x[i] * JUMP).max(0.0).min(W);
+        loc.y = (loc.y + add_y[i] * JUMP).max(0.0).min(H);
     }
 }
 
@@ -73,8 +174,8 @@ fn event(_app: &App, model: &mut Model, event: Event) {
             model.sim_delta += since_last;
             if model.sim_delta > SIM_DELTA {
                 model.sim_delta -= SIM_DELTA;
-                // build_matrix(&mut model.charges, model.matrix;
-                move_charges(&mut model.charges);
+                update_matrix(model);
+                move_charges(model);
             }
         }
         _ => {}
@@ -83,23 +184,23 @@ fn event(_app: &App, model: &mut Model, event: Event) {
 
 fn view(_app: &App, _model: &Model, _frame: Frame) {
     let draw: Draw = _app.draw();
-    for charge in &_model.charges {
+    let it = _model.charges.loc.iter().zip(_model.charges.q.iter());
+    for (loc, charge) in it.clone() {
+        let v = nannou::prelude::Vec2::new(loc.x, loc.y);
         draw.rect()
-            .color(STEELBLUE)
-            .height(charge.q)
-            .width(charge.q)
-            .xy(charge.r + _model.origin);
+            .color(AZURE)
+            .height(*charge)
+            .width(*charge)
+            .xy(v + _model.origin);
     }
 
     for x in 0..X_RES {
         for y in 0..Y_RES {
-            let here = nannou::prelude::Vec2::new(
-                x as f32 * X_STEP,
-                y as f32 * Y_STEP,
-            );
+            let here = nannou::prelude::Vec2::new(x as f32 * X_STEP, y as f32 * Y_STEP);
             let mut potential: f32 = 0.0;
-            for charge in &_model.charges {
-                potential += charge.q / charge.r.distance(here);
+            for (loc, charge) in it.clone() {
+                let v = nannou::prelude::Vec2::new(loc.x, loc.y);
+                potential += charge / v.distance(here);
             }
             potential /= MAX_POT;
             draw.rect()
@@ -107,8 +208,12 @@ fn view(_app: &App, _model: &Model, _frame: Frame) {
                 .width(Y_STEP)
                 .xy(here + _model.origin)
                 .rgba(1.0, 0.0, 0.0, potential);
+            //TODO oversample and average
         }
     }
+    // if _model.slow {
+    //     draw.text("Slow simulation :(").color(WHITE);
+    // }
     draw.background().color(BLACK);
     draw.to_frame(_app, &_frame).unwrap();
 }
